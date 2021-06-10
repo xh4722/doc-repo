ead502b63ce435343ac1081fb84f8985bc320f0e
huboping <bphu@yuansuan.cn>
2021-06-10 14:42:04 +0800

    auto update

--- src/kaiwu-handbook/docusaurus.config.js
+++ src/kaiwu-handbook/docusaurus.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: 'kaiwu-handbook',
-  tagline: '',
+  title: "\u7528\u6237\u624B\u518C",
+  tagline: "\u8FDC\u7B97\u4E91\u5E73\u53F0\u7528\u6237\u624B\u518C",
   url: 'https://your-docusaurus-test-site.com',
   baseUrl: '/',
   favicon: 'img/favicon.ico',
@@ -13,57 +13,42 @@ module.exports = {
         alt: 'ys',
         src: 'img/logo.png'
       },
-      items: [
-        {
-          to: 'docs/home',
-          activeBasePath: 'docs',
-          label: '文档',
-          position: 'left'
-        },
-        {
-          href: 'https://github.com/yskj-fe/doc-platform',
-          label: 'Repo',
-          position: 'right'
-        }
-      ]
+      items: [{
+        to: 'docs/home',
+        activeBasePath: 'docs',
+        label: '文档',
+        position: 'left'
+      }, {
+        href: 'https://github.com/yskj-fe/doc-platform',
+        label: 'Repo',
+        position: 'right'
+      }]
     },
     footer: {
       style: 'dark',
-      links: [
-        {
-          title: 'Doc',
-          items: [
-            {
-              to: 'docs/home',
-              activeBasePath: 'docs',
-              label: '文档'
-            }
-          ]
-        },
-        {
-          title: 'Repo',
-          items: [
-            {
-              href: 'https://github.com/yskj-fe/doc-platform',
-              label: 'Repo'
-            }
-          ]
-        }
-      ],
+      links: [{
+        title: 'Doc',
+        items: [{
+          to: 'docs/home',
+          activeBasePath: 'docs',
+          label: '文档'
+        }]
+      }, {
+        title: 'Repo',
+        items: [{
+          href: 'https://github.com/yskj-fe/doc-platform',
+          label: 'Repo'
+        }]
+      }],
       copyright: `Copyright © ${new Date().getFullYear()} kaiwu-handbook, Inc. Built with Docusaurus.`
     }
   },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js')
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css')
-        }
-      }
-    ]
-  ]
-}
+  presets: [['@docusaurus/preset-classic', {
+    docs: {
+      sidebarPath: require.resolve('./sidebars.js')
+    },
+    theme: {
+      customCss: require.resolve('./src/css/custom.css')
+    }
+  }]]
+};
