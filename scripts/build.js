dd6bbfe9e7e43b225ce4c88704849d9efe4e2c15
huboping <bphu@yuansuan.cn>
2021-06-10 16:10:06 +0800

    auto update

--- scripts/build.js
+++ scripts/build.js
@@ -2,9 +2,13 @@ const path = require("path");
 const { execSync } = require("child_process");
 
 function build() {
-  execSync("docusaurus build", {
-    cwd: path.resolve(__dirname, `../src/${process.env.PROJECT}`),
-  });
+  try {
+    execSync("docusaurus build", {
+      cwd: path.resolve(__dirname, `../src/${process.env.PROJECT}`),
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 build();
