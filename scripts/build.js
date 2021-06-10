309c1f47cb778c7d90d82e3b1f5753daec5f99c5
huboping <bphu@yuansuan.cn>
2021-06-10 16:14:04 +0800

    auto update

--- scripts/build.js
+++ scripts/build.js
@@ -8,7 +8,7 @@ function build() {
     });
   } catch (err) {
     console.error(
-      err.output.map((item) => (item ? item.toString("utf-8") : item))
+      err.output.map((item) => (item ? item.toString("utf-8") : item).join(""))
     );
   }
 }
